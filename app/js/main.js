b53d1d64481d16b7fc6f2e0b10ea9361710ca9b8
Dmitriy <dm.hmara@gmail.com>
2022-12-23 10:36:14 +0200

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,45 @@
+$(function () {
+
+  // $('.master-slider').slick({
+  //   prevArrow: '<button type="button" class="slick-prev"><img src="images/sprite/arrow-slider-prev.svg" alt="стрелка влево"></button>',
+  //   nextArrow: '<button type="button" class="slick-next"><img src="images/sprite/arrow-slider-next.svg" alt="стрелка вправо"></button>',
+  //   dots: true,
+  //   slidesToShow: 3,
+  //   // autoplay: true,
+  //   // autoplaySpeed: 3000,
+
+  // });
+
+  // $(window).scroll(function () {
+  //   const scrolled = $(this).scrollTop();
+  //   if (scrolled > 620) {
+  //     $('.widget').addClass('widget--fixed');
+  //   } else if (scrolled < 620) {
+  //     $('.widget').removeClass('widget--fixed');
+  //   }
+  // });
+
+  // $('.question-faq__title').on('click', function () {
+    
+  //   $('.question-faq__item').removeClass('question-faq__item--active');
+  //   $(this).parent().addClass('question-faq__item--active');
+
+
+  // });
+
+  // $('.review-slider').slick({
+  //   prevArrow: '<button type="button" class="slick-prev"><img src="images/sprite/arrow-slider-prev.svg" alt="стрелка влево"></button>',
+  //   nextArrow: '<button type="button" class="slick-next"><img src="images/sprite/arrow-slider-next.svg" alt="стрелка вправо"></button>',
+  //   dots: false,
+  //   slidesToShow: 2,
+  //   autoplay: true,
+  //   autoplaySpeed: 3000,
+  //   speed: 1500,
+
+  // });
+
+  // // $('.equipment__link').on('click', function () {
+  // //   $('.equipmentbox').removeClass('equipmentbox--active');
+  // // });
+
+})
